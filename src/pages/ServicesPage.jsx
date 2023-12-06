440dc5d9d124b1f79cf39c05aa5d204b8c4d7e88
Paskal Adams <paskaladams@gmail.com>
2023-12-06 22:30:46 +0700

    third  commit

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,5 +1,12 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
+import { testimonialSwiper } from '../data/index';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
 import Services1 from '../assets/img/illustration/Website.svg';
 import Services2 from '../assets/img/illustration/DigitalMarketing.svg';
 import Services3 from '../assets/img/illustration/Consultancy.svg';
@@ -15,7 +22,7 @@ const ServicesPage = () => {
 					</Col>
 				</Row>
 				<Row className="web justify-content-center">
-					<Col lg="3" xs="6" className='mb-5'>
+					<Col lg="3" xs="6" className="mb-5">
 						<img src={Services1} alt="Website" />
 					</Col>
 					<Col lg="9">
@@ -24,7 +31,7 @@ const ServicesPage = () => {
 					</Col>
 				</Row>
 				<Row className="marketing justify-content-center">
-					<Col lg="3" xs="6" className='mb-5'>
+					<Col lg="3" xs="6" className="mb-5">
 						<img src={Services2} alt="Digital Marketing" />
 					</Col>
 					<Col lg="9">
@@ -35,7 +42,7 @@ const ServicesPage = () => {
 					</Col>
 				</Row>
 				<Row className="business justify-content-center">
-					<Col lg="3" xs="6" className='mb-5'>
+					<Col lg="3" xs="6" className="mb-5">
 						<img src={Services3} alt="Consultancy" />
 					</Col>
 					<Col lg="9">
@@ -44,6 +51,58 @@ const ServicesPage = () => {
 					</Col>
 				</Row>
 			</Container>
+			<div className="services-page testimonial w-100">
+				<Row>
+					<Col className='justify-content-center'>
+						<h1 className="text-center fw-bold mb-5">Testimonial</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Swiper
+							slidesPerView={1}
+							spaceBetween={10}
+							pagination={{
+								clickable: true,
+							}}
+							breakpoints={{
+								640: {
+									slidesPerView: 1,
+									spaceBetween: 20,
+								},
+								768: {
+									slidesPerView: 2,
+									spaceBetween: 40,
+								},
+								992: {
+									slidesPerView: 2,
+									spaceBetween: 50,
+								},
+								1200: {
+									slidesPerView: 3,
+									spaceBetween: 50,
+								},
+							}}
+							modules={[Pagination]}
+							className="mySwiper">
+							{testimonialSwiper.map((data) => {
+								return (
+									<SwiperSlide key={data.id} className="rounded-5 ">
+										<p className="desc">{data.desc}</p>
+										<div className="people">
+											<img src={data.image} alt="" />
+											<div>
+												<h5 className="mb-1">{data.name}</h5>
+												<p className="m-0 fw-bold">{data.from}</p>
+											</div>
+										</div>
+									</SwiperSlide>
+								);
+							})}
+						</Swiper>
+					</Col>
+				</Row>
+			</div>
 		</div>
 	);
 };
